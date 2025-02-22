fc044ce096208e74d92c878fe8dd6d199bb5bf60
Vindusvisker <marcruud@gmail.com>
2025-02-22 12:06:26 +0100

    Initial commit

--- src/app/404/page.tsx
+++ src/app/404/page.tsx
@@ -24,7 +24,7 @@ function NotFoundContent() {
   );
 }
 
-export default function NotFound() {
+export default function NotFoundPage() {
   return (
     <Suspense fallback={<div>Loading...</div>}>
       <NotFoundContent />
